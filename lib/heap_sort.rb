ba7eda6de678d29f76266a51c5f633fc40fcf5b3
Allen Chen <alchen1231@gmail.com>
2017-06-14 22:41:21 -0700

    binary heap

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -2,5 +2,14 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    sorted = []
+    # heapy = BinaryMinHeap.new
+    map do |num|
+      # heapy.push(num)
+    end
+    until heapy.count == 0
+      sorted.push(heapy.extract)
+    end
+    sorted
   end
 end
