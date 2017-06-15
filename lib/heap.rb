e09829d16d4a3811c893637824b1dc306e89f80e
Allen Chen <alchen1231@gmail.com>
2017-06-14 20:41:00 -0700

    binary heap

--- lib/heap.rb
+++ lib/heap.rb
@@ -6,9 +6,11 @@ class BinaryMinHeap
   end
 
   def count
+    @store.length
   end
 
   def extract
+
   end
 
   def peek
@@ -16,15 +18,25 @@ class BinaryMinHeap
   end
 
   def push(val)
+    @store << val
+    #until parent < val, keep swapping with parent
   end
 
   public
   def self.child_indices(len, parent_index)
+    children = []
+    (1..2).each do |i|
+      children << ((2 * parent_index) + i)
+    end
+    children.select {|idx| idx < len}
     # 2i + 1 & 2i + 2
+
   end
 
   def self.parent_index(child_index)
     # (i-1)/2
+    raise 'root has no parent' if child_index == 0
+    (child_index - 1) / 2
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
